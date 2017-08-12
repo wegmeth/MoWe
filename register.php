924d07750f6fadd9727fd0b914766ef922db299a
Muffin <marvin.dick96@web.de>
2017-08-12 18:25:02 +0200

    config.php geaddet, register.php angefangen

--- register.php
+++ register.php
@@ -1,4 +1,53 @@
 <?php
+session_start();
+
+require("config.php");
+  $err_msg;
+  $warn_msg;
+
+  //Überprüfen, ob die Angaben vollständig sind
+  $required = array('E-Mail-Adresse', 'Passwort', 'Retype-Passwort', 'Username');
+  $error = false;
+
+  foreach($required as $field)
+  {
+      if(empty($_POST[$field]))
+      {
+          $error = true;
+      }
+  }
+  if($error)
+    $warn_msg = "Alle Felder müssen gefüllt werden.";
+  else
+  {
+  //Benutzer
+      $email = $_POST['E-Mail-Adresse'];
+      $username = $_POST['Username'];
+      $pass = $_POST['Passwort'];
+      $pass_retype = $_POST['Retype-Passwort'];
+      $hash = password_hash($pass, PASSWORD_DEFAULT);
+
+      if($pass != $pass_retype)
+      {
+          $err_msg = "Passwort stimmt nicht überein!";
+      }
+      else
+      {
+          try {
+              $pdo = new PDO( $db_dsn, $db_username, $db_passwrd);
+
+              //TODO: User-Insert
+
+          } catch (PDOException $e){
+              print "Error!: " . $e->getMessage() . "<br/>";
+              die();
+          }
+
+
+      }
+  }
+
 include("register.html");
+
 ?>
 
